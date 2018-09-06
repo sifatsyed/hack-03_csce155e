d9c1f65762888e591a5628831ad81a3825cc6c4c
sifatsyed <42530377+sifatsyed@users.noreply.github.com>
2018-09-06 01:04:48 -0500

    Add files via upload

---
+++ cellPlan.c
@@ -0,0 +1,50 @@
+/**
+ * Author: Sifat Syed
+ *
+ * This program calculates the average daily usage
+ * of their mobile data plan. It tell the costumer
+ * whether they are over, under or right on the
+ * average daily usage if their plan.
+ */
+
+ #include<stdlib.h>
+ #include<stdio.h>
+ #include<math.h>
+
+ int main(int argc, char **argv) {
+
+  int dayNumber, daysLeft;
+  double numberOfGBPlan, totalGBUsed, averageDailyUse, averageUsePerDay, planExceededCalculation, planExceeded,
+        warningGBUse, totalGBULeft, atLeastUse, planDeceeded, warningShortGBUse;
+
+  if(argc!=4){
+    printf("Error:Provide the correct number of data\n");
+    exit(1);
+  }
+  numberOfGBPlan = atof(argv[1]);
+  dayNumber = atoi(argv[2]);
+  totalGBUsed = atof(argv[3]);
+
+   daysLeft = 30 - dayNumber;
+   averageUsePerDay = totalGBUsed/dayNumber;
+   averageDailyUse = numberOfGBPlan/30 ;
+   planExceededCalculation = numberOfGBPlan - totalGBUsed + (daysLeft*averageUsePerDay);
+   planExceeded = planExceededCalculation - numberOfGBPlan;
+   warningGBUse = totalGBULeft/daysLeft;
+
+  printf("%d days used, %d days remaining.\n",dayNumber, daysLeft);
+  printf("Average daily use: %f GB/day\n", averageUsePerDay);
+
+  if(averageUsePerDay>averageDailyUse) {
+    printf("You are EXCEEDING your average daily use (%f GB/day).\n", averageDailyUse);
+    printf("Continuing this high usage, you will exceed your data plan by %f GB.\n", planExceeded);
+    printf("To stay below your data plan use no more than %f GB per day\n",warningGBUse);
+}
+ /*else if(averageUsePerDay<averageDailyUse) {
+    printf("You are DECEEDING your average daily use (%f GB/day).\n", averageDailyUse);
+    printf("Continuing this low usage, you will deceed your data plan by %f GB.\n", planDeceeded);
+    printf("To stay at right limit of your data plan, use atleast %f GB per day\n",warningShortGBUse);
+
+}*/ else {
+  printf("You are using your plan at the right average daily use, so no need to worry.\n", );
+}
